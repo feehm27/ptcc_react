9f810bf403c76198a10fe92df31cf0515ee39743
Fernanda <fernanda.lightbase@sebraemg.com.br>
2021-11-10 11:12:36 -0300

    Proteção de acesso as rotas

--- src/helpers/Route/ProtectedRoute.js
+++ src/helpers/Route/ProtectedRoute.js
@@ -1,14 +1,13 @@
-// import { useContext } from 'react';
-// import { Navigate, Route } from 'react-router-dom';
-// import { UserContext } from 'src/contexts/UserContext';
-import { Route } from 'react-router-dom';
+import { useContext } from 'react';
+import { Navigate, Route } from 'react-router-dom';
+import { UserContext } from 'src/contexts/UserContext';
 
 const ProtectedRoute = (props) => {
-  return <Route {...props} />;
-  // const { login } = useContext(UserContext)//;
-  //   if (login === true) return <Route {...props} />;
-  //   else if (login === false) return <Navigate to="/login" />;
-  //   else return null/
+  const { login } = useContext(UserContext);
+
+  if (login === true) return <Route {...props} />;
+  else if (login === false) return <Navigate to="/login" />;
+  else return null;
 };
 
 export default ProtectedRoute;
