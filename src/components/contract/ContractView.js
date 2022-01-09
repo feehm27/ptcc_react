e9ca8a66031c2ecede3ada06c648077a8f5a070d
Fernanda <fernanda.lightbase@sebraemg.com.br>
2022-01-09 12:11:02 -0300

    Criação inicial de visualização do contrato gerado

---
+++ src/components/contract/ContractView.js
@@ -0,0 +1,98 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardHeader,
+  CardContent,
+  Divider,
+  Stack
+} from '@material-ui/core';
+import { useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+
+const ContractView = () => {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+  const url =
+    'https://cors-anywhere.herokuapp.com/https://advoguez-images.s3.sa-east-1.amazonaws.com/H93QRT6R.pdf';
+
+  document.addEventListener('contextmenu', (event) => {
+    event.preventDefault();
+  });
+
+  function onDocumentLoadSuccess({ pages }) {
+    setNumPages(pages);
+    setPageNumber(1);
+  }
+
+  function changePage(offset) {
+    setPageNumber((prevPageNumber) => prevPageNumber + offset);
+  }
+
+  function previousPage() {
+    changePage(-1);
+  }
+
+  function nextPage() {
+    changePage(1);
+  }
+
+  return (
+    <Card
+      sx={{
+        m: '30px'
+      }}
+    >
+      <CardHeader title="Visualização do Contrato" />
+      <Divider />
+      <CardContent sx={{}}>
+        <div style={{ textAlign: 'center' }}>
+          <div className="pagec">
+            Página {pageNumber || (numPages ? 1 : '--')} de {numPages || '--'}
+          </div>
+        </div>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            p: 2
+          }}
+        >
+          <Stack direction="row" spacing={2}>
+            <Button
+              onClick={() => previousPage()}
+              disabled={pageNumber <= 1}
+              autoFocuscolor="primary"
+              variant="contained"
+              type="button"
+            >
+              Página Anterior
+            </Button>
+            <Button
+              type="button"
+              disabled={pageNumber >= numPages}
+              onClick={() => nextPage()}
+              variant="contained"
+            >
+              Proxima Página
+            </Button>
+          </Stack>
+        </Box>
+        <div
+          style={{
+            marginLeft: '22%'
+          }}
+        >
+          <Document file={url} onLoadSuccess={onDocumentLoadSuccess}>
+            <Page pageNumber={pageNumber} />
+          </Document>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ContractView;
